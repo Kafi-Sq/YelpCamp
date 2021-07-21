6634aded21ccc9d11aea7afcb51a060a1a06fc38
Hassen Hassen <xasansq@gmail.com>
2021-07-20 21:18:36 -0500

    ready to deploy

--- app.js
+++ app.js
@@ -20,21 +20,24 @@ const mongoSanitize = require('express-mongo-sanitize');
 
 const userRoutes = require('./routes/users')
 const campgroundsRoutes = require('./routes/campground')
-const reviewsRoutes = require('./routes/reviews')
-// const dbUrl = process.env.DB_URL
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+const reviewsRoutes = require('./routes/reviews');
+
+const MongoDBStore = require("connect-mongo")(session);
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-    useNewUrlParser: true,
     useFindAndModify: false
-})
-mongoose.connection
-    .once('open', () => {
-        console.log('Database Connected')
-    })
-    .on('error', (err) => {
-        console.log(err)
-    })
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 app.engine('ejs', engine)
 app.set('view engine', 'ejs')
@@ -89,13 +92,27 @@ app.use(
     })
 );
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
+const store = new MongoDBStore({
+    url: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+})
+
 const sessionConfig = {
-    name: 'wthits',
-    secret: 'basicSecret',
+    store,
+    name: 'session',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
